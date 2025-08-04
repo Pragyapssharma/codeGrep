33cea880f4013f94cc13c0c12a18b16b6d0e5550
pragya sharma <pragyasharma1209@gmail.com>
2025-08-04 10:51:51 +0530

    match one or more times code

--- src/main/java/RegexMatcher.java
+++ src/main/java/RegexMatcher.java
@@ -1,113 +1,97 @@
 import java.util.*;
 
 public class RegexMatcher {
-	private final List<Token> tokens;
-	private final boolean anchored;
-	private final boolean anchoredEnd;
+    private final List<Token> tokens;
+    private final boolean anchored;
+    private final boolean anchoredEnd;
 
-	public RegexMatcher(String pattern) {
-		if (pattern.startsWith("^")) {
-			this.anchored = true;
-			pattern = pattern.substring(1); // remove the anchor for tokenization
-		} else {
-			this.anchored = false;
-		}
-		
-		 if (pattern.endsWith("$")) {
-			    this.anchoredEnd = true;
-			    pattern = pattern.substring(0, pattern.length() - 1);
-			  } else {
-			    this.anchoredEnd = false;
-			  }
-		
-		this.tokens = tokenize(pattern);
-	}
+    public RegexMatcher(String pattern) {
+        if (pattern.startsWith("^")) {
+            this.anchored = true;
+            pattern = pattern.substring(1);
+        } else {
+            this.anchored = false;
+        }
 
-	public boolean matches(String input) {
-		if (anchored) {
-			return matchesAt(input, 0); // only match at start
-		} else {
-			for (int i = 0; i <= input.length() - tokens.size(); i++) {
-				if (matchesAt(input, i)) {
-					return true;
-				}
-			}
-			return false;
-		}
-	}
+        if (pattern.endsWith("$")) {
+            this.anchoredEnd = true;
+            pattern = pattern.substring(0, pattern.length() - 1);
+        } else {
+            this.anchoredEnd = false;
+        }
 
-	private boolean matchesAt(String input, int start) {
-		  for (int i = 0; i < tokens.size(); i++) {
-		    if (start + i >= input.length()) 
-		    	return false;
-		    if (!tokens.get(i).matches(input.charAt(start + i))) 
-		    	return false;
-		  }
+        this.tokens = tokenize(pattern);
+    }
 
-		  return !anchoredEnd || (start + tokens.size() == input.length());
-		}
+    public boolean matches(String input) {
+        if (anchored) {
+            return matchesAt(input, 0);
+        } else {
+            for (int i = 0; i <= input.length(); i++) {
+                if (matchesAt(input, i)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 
-	private List<Token> tokenize(String pattern) {
-		List<Token> tokens = new ArrayList<>();
-		for (int i = 0; i < pattern.length();) {
-			char c = pattern.charAt(i);
-			if (c == '\\' && i + 1 < pattern.length()) {
-				char next = pattern.charAt(i + 1);
-				if (next == 'd') {
-					tokens.add(new Token(TokenType.DIGIT, ""));
-				} else if (next == 'w') {
-					tokens.add(new Token(TokenType.WORD, ""));
-				} else {
-					tokens.add(new Token(TokenType.CHAR, String.valueOf(next)));
-				}
-				i += 2;
-			} else if (c == '[') {
-				int end = pattern.indexOf(']', i);
-				if (end == -1)
-					throw new RuntimeException("Unclosed [");
-				String group = pattern.substring(i + 1, end);
-				if (group.startsWith("^")) {
-					tokens.add(new Token(TokenType.NEGATIVE_GROUP, group.substring(1)));
-				} else {
-					tokens.add(new Token(TokenType.POSITIVE_GROUP, group));
-				}
-				i = end + 1;
-			} else {
-				tokens.add(new Token(TokenType.CHAR, String.valueOf(c)));
-				i++;
-			}
-		}
-		return tokens;
-	}
+    private boolean matchesAt(String input, int start) {
+        int i = start;
+        for (Token token : tokens) {
+            if (token.quantifier == Token.Quantifier.ONE) {
+                if (i >= input.length() || !token.matches(input.charAt(i))) return false;
+                i++;
+            } else if (token.quantifier == Token.Quantifier.ONE_OR_MORE) {
+                int count = 0;
+                while (i < input.length() && token.matches(input.charAt(i))) {
+                    i++;
+                    count++;
+                }
+                if (count == 0) return false;
+            }
+        }
+        return !anchoredEnd || (i == input.length());
+    }
 
-	enum TokenType {
-		DIGIT, WORD, CHAR, POSITIVE_GROUP, NEGATIVE_GROUP
-	}
+    private List<Token> tokenize(String pattern) {
+        List<Token> tokens = new ArrayList<>();
+        for (int i = 0; i < pattern.length();) {
+            char c = pattern.charAt(i);
+            Token token;
 
-	static class Token {
-		TokenType type;
-		String value;
+            if (c == '\\' && i + 1 < pattern.length()) {
+                char next = pattern.charAt(i + 1);
+                if (next == 'd') {
+                    token = new Token(Token.TokenType.DIGIT, "");
+                } else if (next == 'w') {
+                    token = new Token(Token.TokenType.WORD, "");
+                } else {
+                    token = new Token(Token.TokenType.CHAR, String.valueOf(next));
+                }
+                i += 2;
+            } else if (c == '[') {
+                int end = pattern.indexOf(']', i);
+                if (end == -1) throw new RuntimeException("Unclosed [");
+                String group = pattern.substring(i + 1, end);
+                if (group.startsWith("^")) {
+                    token = new Token(Token.TokenType.NEGATIVE_GROUP, group.substring(1));
+                } else {
+                    token = new Token(Token.TokenType.POSITIVE_GROUP, group);
+                }
+                i = end + 1;
+            } else {
+                token = new Token(Token.TokenType.CHAR, String.valueOf(c));
+                i++;
+            }
 
-		Token(TokenType type, String value) {
-			this.type = type;
-			this.value = value;
-		}
+            if (i < pattern.length() && pattern.charAt(i) == '+') {
+                token.quantifier = Token.Quantifier.ONE_OR_MORE;
+                i++;
+            }
 
-		boolean matches(char c) {
-			switch (type) {
-			case DIGIT:
-				return Character.isDigit(c);
-			case WORD:
-				return Character.isLetterOrDigit(c) || c == '_';
-			case CHAR:
-				return c == value.charAt(0);
-			case POSITIVE_GROUP:
-				return value.indexOf(c) != -1;
-			case NEGATIVE_GROUP:
-				return value.indexOf(c) == -1;
-			default:
-				return false;
-			}
-		}
-	}
+            tokens.add(token);
+        }
+        return tokens;
+    }
 }
---
+++ src/main/java/Token.java
@@ -0,0 +1,40 @@
+public class Token {
+    enum TokenType {
+        DIGIT, WORD, CHAR, POSITIVE_GROUP, NEGATIVE_GROUP
+    }
+
+    enum Quantifier {
+        ONE, ONE_OR_MORE
+    }
+
+    TokenType type;
+    String value;
+    Quantifier quantifier;
+
+    public Token(TokenType type, String value) {
+        this(type, value, Quantifier.ONE);
+    }
+
+    public Token(TokenType type, String value, Quantifier quantifier) {
+        this.type = type;
+        this.value = value;
+        this.quantifier = quantifier;
+    }
+
+    public boolean matches(char c) {
+        switch (type) {
+            case DIGIT:
+                return Character.isDigit(c);
+            case WORD:
+                return Character.isLetterOrDigit(c) || c == '_';
+            case CHAR:
+                return c == value.charAt(0);
+            case POSITIVE_GROUP:
+                return value.indexOf(c) != -1;
+            case NEGATIVE_GROUP:
+                return value.indexOf(c) == -1;
+            default:
+                return false;
+        }
+    }
+}
